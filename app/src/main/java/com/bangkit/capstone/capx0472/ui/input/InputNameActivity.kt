60999bb62af160eaf37fe1efb57f2834e137308d
Muhamad Irwan <muhamadirwan99@gmail.com>
2021-05-31 14:37:57 +0700

    Minor fix splashscreen and input activity

--- app/src/main/java/com/bangkit/capstone/capx0472/ui/input/InputNameActivity.kt
+++ app/src/main/java/com/bangkit/capstone/capx0472/ui/input/InputNameActivity.kt
@@ -8,5 +8,7 @@ class InputNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_name)
+
+        supportActionBar?.title = ""
     }
 }
